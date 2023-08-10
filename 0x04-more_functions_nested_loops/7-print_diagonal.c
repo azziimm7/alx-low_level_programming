dfd5090618e5028876c0939bd3fc8b5812d15d5c
azziimm7 <azzah31@outlook.com>
2023-08-10 00:07:15 -0700

    diagonals ;

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,22 +5,23 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	int c, i;
 
-		for (i = 0; i < n; i++)
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
+			_putchar(' ');
+			i--;
 		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
 	}
+	if (c < 1)
+		_putchar('\n');
 }
