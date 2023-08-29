5a1d4e91409365ed7dc85bd7dab34af481cbf7aa
azziimm7 <Azza>
2023-08-29 01:12:25 -0700

    progressing

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,9 +17,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	ptr->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = ptr;
 		return (*head);
-
+	}
 	ptr->next = *head;
 	*head = ptr;
 	return (*head);
