06ed33a5920f2aa00eaeb7caf887119a5957080e
azziimm7 <Azza>
2023-08-29 00:51:31 -0700

    profressing

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - count the elements in the list
+ * @h: start point of the list
+ * Return: number of the elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
