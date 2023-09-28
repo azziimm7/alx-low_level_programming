2e1335b6bb8093884939943862bd1a378c0b397f
azziimm7 <azzah31@outlook.com>
2023-09-28 11:46:09 -0700

    l

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 /**
  * binary_to_uint - convert the binary number to a decimal
  * @b: points to a string of 0's and 1's
- * @i: the decimal format of the binary
+ * @i: is the variable to hold the decimal format
  * Return: decimal format of the binary
  */
 unsigned int binary_to_uint(const char *b)
@@ -12,10 +12,12 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
+
 	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
+
 		i = i * 2 + (*b++ - '0');
 	}
 	return (i);
